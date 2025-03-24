0372efdd9d24e6bfaa9f9a530de9c7ec0f002126
JongwonLee <gudtksfl@gmail.com>
2025-03-24 19:17:25 +0900

    feat: 회원가입 유효성 검사 추가

--- src/main/java/com/toyblog/dto/CreateUserRequestDTO.java
+++ src/main/java/com/toyblog/dto/CreateUserRequestDTO.java
@@ -1,11 +1,18 @@
 package com.toyblog.dto;
 
+
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 public record CreateUserRequestDTO(
-        String id,
+        @NotBlank @Size(min=6,max=30) String id,
+        @NotBlank
+        @Size(min=12,max=50)
+        @Pattern(regexp = "^(?=(.*[a-zA-Z]){2,})(?=(.*\\d){2,})(?=(.*[!@#$%^&*]){2,}).{12,50}$")
         String password,
-        String email,
-        String nickname
+        @NotBlank @Email String email,
+        @NotBlank @Size(max=50) String nickname
 ) {
 }
